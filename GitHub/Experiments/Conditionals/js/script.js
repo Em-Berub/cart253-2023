29f601da1334bc8839a560a7072da447bfeba68c
SoniferousVacuum <emman_97@hotmail.ca>
2023-09-23 16:17:12 -0400

    Creates a circle that increases and decreases in size,, moving forward and backward

--- GitHub/Experiments/Conditionals/js/script.js
+++ GitHub/Experiments/Conditionals/js/script.js
@@ -1,32 +1,59 @@
-/**
+  /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
 function preload() {
 
 }
-
-
-/**
- * Description of setup
-*/
-function setup() {
+let circle = {
+    x:0,
+    y:250,
+    speed:7,
+    size:5
 
 }
 
 
-/**
- * Description of draw()
-*/
-function draw() {
+function setup() {
+createCanvas(500,500)
 
+}
+
+
+
+function draw() {
+    background(0)
+    fill(255,255,255)
+circle.x += circle.speed
+
+// if (mouseX > width/2){
+//     circle.size= map(circle.x, 0,width, 0 ,25 )
+//  }
+//  if (mouseX < width/2){
+//     circle.x= constrain(circle.x, 100 ,400 )
+//  }
+ if (circle.x<=width/2){
+    circle.size= map(circle.x, 0,width, 0 ,50 )
+
+}
+if (circle.x>width/2){
+    circle.size= map(circle.x, 0,width, 50 ,0 )
+
+}
+
+if (circle.x>width){
+    circle.speed = -circle.speed
+}
+
+if (circle.x<0){
+    circle.speed = -circle.speed
+}
+
+
+ellipse(circle.x, circle.y, circle.size)
 }
