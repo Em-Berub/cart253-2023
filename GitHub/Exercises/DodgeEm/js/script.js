cece8fb5029233a0ed01d816083d6d9f7f8bb906
SoniferousVacuum <emman_97@hotmail.ca>
2023-09-26 00:56:53 -0400

    title, background, fctn mousepressed,canvas limit

--- GitHub/Exercises/DodgeEm/js/script.js
+++ GitHub/Exercises/DodgeEm/js/script.js
@@ -9,38 +9,269 @@ function preload() {
 }
 
 let circle = {
-    x:25,
-    y:250,
-    speed:2,
-    size:2
+    x:0,
+    y:200,
+    vx:30,
+    size:50
 
 }
 
+let sha = {
+  x:0,
+  y:200,
+  vx:20,
+  size:50
+
+}
 
 function setup() {
-createCanvas(500, windowHeight)
- 
-background(120,122,122)
-fill (255,100,100)
-  textSize(40);
-textAlign(CENTER);
-textStyle(BOLDITALIC);
-text('DODGE THE C', 250, 50);
-textAlign(CENTER);
-strokeWeight(0);
+createCanvas(500, windowHeight- 100)
 
-image(img, 0, 200,50,50);
+
+
+
+
+ 
+
+
+
 }
 
 
 
 function draw() {
+  background(140,132,132)
+  push()
+strokeWeight(2)
+stroke(0)
+fill(43, 153, 142)
+ellipse(mouseX,mouseY,20,20)
+cursor(`cell`)
+pop()
+fill (255,100,100)
+stroke(255)
+strokeWeight(6);
+
+  textSize(35);
+textAlign(CENTER);
+textStyle(BOLDITALIC);
+text('DODGE THE PARTICULES', 250, 50);
+textAlign(CENTER);
+stroke (0)
+line (35,60,470, 60)
+
+
+
+push ();
+
+  for (let i = 0; i < 5000; i++) {
+          let x = random(0, width);
+          let y = random(0, height);
+          stroke(255);
+          strokeWeight(1)
+          point(x,y)
+        }
+
+pop ();
+  
+// image(img, 0, 0,50,50);
+
+ 
+
+// }
+
+
+
+if (circle.y > height){
+  // circle.y = -circle.y - 10 
+  circle.vx = -circle.vx
+  // circle.y = random(0,height)
+  circle.x =random(0,width)  
+}
+ if  (circle.y < 0){
+  circle.vx =-circle.vx
+  circle.x=random(0,width)
+  // circle.y = random(0,height)
+}
+
+
+circle.y = circle.y + circle.vx
+// circle.x = circle.x + circle.vx
+
+// for( let i = 0 ; i<1; i++){
+  sha.x = sha.x + sha.vx
+  ellipse(sha.x,sha.y,sha.size)
+  // sha.x = sha.x+ random(0,5)
+  // sha.y = sha.y + random(0,5)
+  if (sha.x > width){
+    sha.vx = -sha.vx
+    // circle.y = -circle.y - 10 
+    
+    sha.y = random(0,height)
+    // sha.x =random(0,width)  
+  }
+   if  (sha.x < 0){
+    
+    sha.vx =-sha.vx
+    // sha.x=random(0,width)
+    sha.y = random(0,height)
+    // circle.y = random(0,height)
+  }
+
+
+
+ellipse(circle.x,circle.y,circle.size)
+// frameRate(10)
+
+
+
+// ********************
+
+
+
+
+
+if (mouseX > width || mouseX < 0 || mouseY > height ||mouseY < 0){
+  noLoop()
+}
+
+
+let d = dist(mouseX, mouseY, circle.x, circle.y);
+if (d < 20) {
+  print(d)
+// stroke(0)
+// strokeWeight(2)
+// line(mouseX,mouseY,circle.x, circle.y)
+noLoop()
+}
+
+
+
+let dis = dist(mouseX, mouseY, sha.x, sha.y);
+if (dis < 20) {
+  print(dis)
+// stroke(0)
+// strokeWeight(2)
+// line(mouseX,mouseY,sha.x, sha.y)
+  noLoop()
+}
+
+
+
+
+
+}
+
+
+// *************************
+
+function mousePressed() {
+  loop();
+}
+function keyPressed() {
+  loop();
+}
+
+
+
+// if (circle.x > width/3){
+//   // circle.y = -circle.y - 10 
+//   circle.vx = -circle.vx
+//   circle.x =random(0,width)  
+// }
+// if   (circle.x < width/3){
+//   circle.vx =-circle.vx
+//   circle.y=random(0,height)
+// }
+
+
+
+
+
+
+// else if (mouseX >250) {
+
+//     circle.x =random (150,width) 
+    
+//   circle.y = random(150,height)
+//   // frameRate(5)
+// }
+
+
+
+
+
+
+
+// for(let i=0; i < 2; i++){
+
+//   fill(0)s
+//   // ellipse(circle.x,circle.y,circle.size)
+
+//   // circle.x = circle.x + 10
+//   // circle.y = circle.y + 10
+// if (circle.x > 500){
+//     circle.y = -circle.y - 10    
+// }
+// if   (circle.x < 0){
+//     circle.y =circle.y + 100
+// }
+
+
+// // circle.x = circle.x + circle.speed
+// // ellipse(circle.x,circle.y,circle.size)
+//   // circle.x=random(0,500)
+//   // circle.y=random(70,height)
+//   // noLoop()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0 ; i< 10; i++){
+    
+//     circle.y = circle.y +20
+//     circle.x = circle.x +50
+//     if (circle.x > 500){
+//         circle.speed = -circle.speed
+//     }
+//     if  (circle.x < 0){
+//         circle.speed =-circle.speed
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   // background(120,122,122)
 
-  cursor(`cell`)
-stroke (255)
+  
+
 // let x;
 // let y;
 // x=200
@@ -52,62 +283,40 @@ stroke (255)
 //         point(x, y);
 // }
 
-    fill(255,200,200)
-    push() 
-strokeWeight(4)
-line (50,60,450, 60)
-pop ()
-push()
-strokeWeight(1)
-// circle.x+=circle.speed
-fill(0)
-ellipse(mouseX,mouseY,circle.size)
-pop()
 
-for(let i=0; i< 100; i++){
-  ellipse(circle.x,circle.y,circle.size)
-  circle.x=random(0,500)
-  circle.y=random(0,height)
-  // noLoop()
-}
-if (mouseX = circle.x + 20){
-  fill(0)
-  rect(100,100,100,100)
+
+
+
+
+// push()
+
+// pop()
+// if ( mouseY = circle.y+20){
+//   fill(0)
+//   // rect(100,100,100,100)
   
-}
+// }
 
 // push()
 // strokeWeight(2)
-// frameRate(30); 
-// drawEllipse();
+// frameRate(60); 
+
 // pop()
 
 
-// if (circle.x > 500){
-//     circle.speed = -circle.speed
-// }
-// if  (circle.x < 0){
-//     circle.speed =-circle.speed
-// }
 
 
-// for (let i = 0 ; i< 10; i++){
-//     
-//     circle.y = circle.y +20
-//     circle.x = circle.x +50
-//     if (circle.x > 500){
-//         circle.speed = -circle.speed
+    // let d = dist(circle.x, circle.y, 500, 700);
+
+
+
+    // drawEllipse();
+ 
+
+// function drawEllipse(){//randomly draws the elliptic shapes onto the background
+
+//     ellipse(random(width), random(90,height), random(3,10), random(0,20));
 //     }
-//     if  (circle.x < 0){
-//         circle.speed =-circle.speed
-//     }
-// }
-}
-
-function drawEllipse(){//randomly draws the elliptic shapes onto the background
-
-    ellipse(random(width), random(90,height), random(3,10), random(0,20));
-    }
 
 
 
@@ -206,4 +415,4 @@ function drawEllipse(){//randomly draws the elliptic shapes onto the background
 //     // Display user
 //     fill(user.fill);
 //     ellipse(user.x, user.y, user.size);
-//   }
+  // }
